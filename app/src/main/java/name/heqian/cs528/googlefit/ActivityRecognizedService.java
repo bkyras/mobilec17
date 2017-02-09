6438b3c09336ec0fe2b15d9761f29771e20bffbb
Gao <ggao@wpi.edu>
2017-02-08 20:07:00 -0500

    Logic to send messages for real

--- app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
+++ app/src/main/java/name/heqian/cs528/googlefit/ActivityRecognizedService.java
@@ -1,26 +1,24 @@
 package name.heqian.cs528.googlefit;
 
 import android.app.IntentService;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.TextView;
 import android.util.Log;
-import android.view.View;
+import android.content.Intent;
 
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
+
 
 /**
  * Created by Paul on 2/1/16.
  */
 public class ActivityRecognizedService extends IntentService {
-    DBHelper mDbHelper = new DBHelper(getApplicationContext());
 
     public ActivityRecognizedService() {
         super("ActivityRecognizedService");
@@ -38,54 +36,49 @@ public class ActivityRecognizedService extends IntentService {
         }
     }
 
-    private void addToDatabase(String activity) {
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        //get current time
-        String timestamp = DateFormat.getTimeInstance().format(DateFormat.LONG);
-        values.put("timestamp", timestamp);
-        values.put("activity", activity);
 
-        db.insert("ActivityTracker", null, values);
-    }
 
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
         for( DetectedActivity activity : probableActivities ) {
             switch( activity.getType() ) {
                 case DetectedActivity.IN_VEHICLE: {
                     Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.ON_BICYCLE: {
-                    Log.e( "ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.ON_FOOT: {
-                    Log.e( "ActivityRecogition", "On Foot: " + activity.getConfidence() );
+                        if( activity.getConfidence() >= 75 ) {
+                        Intent shareIntent = new Intent("msg");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, "Drive");
+                        System.out.println("Driving message sent");
+                        sendMessage(shareIntent);
+                    }
                     break;
                 }
                 case DetectedActivity.RUNNING: {
                     Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
+                    if( activity.getConfidence() >= 75 ) {
+                        Intent shareIntent = new Intent("msg");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, "Run");
+                        System.out.println("Running message sent");
+                        sendMessage(shareIntent);
+                    }
                     break;
                 }
                 case DetectedActivity.STILL: {
                     Log.e( "ActivityRecogition", "Still: " + activity.getConfidence() );
-                    break;
-                }
-                case DetectedActivity.TILTING: {
-                    Log.e( "ActivityRecogition", "Tilting: " + activity.getConfidence() );
+                    if( activity.getConfidence() >= 50 ) {
+                        Intent shareIntent = new Intent("msg");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, "Stand");
+                        System.out.println("Still message sent");
+                        sendMessage(shareIntent);
+                    }
                     break;
                 }
                 case DetectedActivity.WALKING: {
                     Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
                     if( activity.getConfidence() >= 75 ) {
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText( "Are you walking?" );
-                        builder.setSmallIcon( R.mipmap.ic_launcher );
-                        builder.setContentTitle( getString( R.string.app_name ) );
-                        NotificationManagerCompat.from(this).notify(0, builder.build());
-                        addToDatabase("walking");
+                        Intent shareIntent = new Intent("msg");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT,"Walk");
+                        System.out.println("Walking message sent");
+                        sendMessage(shareIntent);
                     }
                     break;
                 }
@@ -96,4 +89,10 @@ public class ActivityRecognizedService extends IntentService {
             }
         }
     }
+
+    public void sendMessage(Intent intent){
+        System.out.println("We in side sendMessage");
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
 }
--- app/src/main/java/name/heqian/cs528/googlefit/MainActivity.java
+++ app/src/main/java/name/heqian/cs528/googlefit/MainActivity.java
@@ -1,13 +1,18 @@
 package name.heqian.cs528.googlefit;
 
 import android.app.PendingIntent;
-import android.content.ContentValues;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.widget.ImageView;
 import android.os.Bundle;
-import android.database.sqlite.*;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -24,6 +29,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if(Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendImage(intent);
+            }
+        }
+        */
+
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
                 .addConnectionCallbacks(this)
@@ -31,6 +48,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .build();
 
         mApiClient.connect();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("msg"));
     }
 
     @Override
@@ -49,4 +69,59 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    // Handles an image being passed through an intent
+    void handleSendImage(Intent intent){
+        PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( mApiClient, 3000, pendingIntent );
+
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null){
+            if(sharedText.equals("Drive")){
+                ImageView pic = (ImageView) findViewById(R.id.ImageView);
+                pic.setImageResource(R.drawable.in_vehicle);
+            } else if(sharedText.equals("Run")){
+                ImageView pic = (ImageView) findViewById(R.id.ImageView);
+                pic.setImageResource(R.drawable.running);
+            } else if(sharedText.equals("Walk")){
+                ImageView pic = (ImageView) findViewById(R.id.ImageView);
+                pic.setImageResource(R.drawable.walking);
+            } else if (sharedText.equals("Stand")){
+                ImageView pic = (ImageView) findViewById(R.id.ImageView);
+                pic.setImageResource(R.drawable.still);
+            }
+        }
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("Inside receiver");
+            String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ImageView pic = (ImageView) findViewById(R.id.ImageView);
+            TextView actText = (TextView) findViewById(R.id.textView2);
+            if (sharedText != null){
+                if(sharedText.equals("Drive")){
+                    System.out.println("Driving message received");
+                    pic.setImageResource(R.drawable.in_vehicle);
+                    actText.setText(R.string.driving);
+                } else if(sharedText.equals("Run")){
+                    System.out.println("Running message received");
+                    pic.setImageResource(R.drawable.running);
+                    actText.setText(R.string.running);
+                } else if(sharedText.equals("Walk")){
+                    System.out.println("Walking message received");
+                    pic.setImageResource(R.drawable.walking);
+                    actText.setText(R.string.walking);
+                } else if (sharedText.equals("Stand")){
+                    System.out.println("Standing message received");
+                    pic.setImageResource(R.drawable.still);
+                    actText.setText(R.string.still);
+                }
+            }
+
+
+        }
+    };
+
 }
