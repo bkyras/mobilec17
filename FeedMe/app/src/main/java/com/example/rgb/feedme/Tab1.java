22abc3effc7fa2c9f34b4804b2a7c13d04327859
Sheridan <bsheridan@wpi.edu>
2017-02-26 16:49:01 -0500

    Loads from db immediately, layout changes

--- FeedMe/app/src/main/java/com/example/rgb/feedme/Tab1.java
+++ FeedMe/app/src/main/java/com/example/rgb/feedme/Tab1.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 public class Tab1 extends android.support.v4.app.Fragment {
 
     DBHelper dbHelper;
+    View v;
 
     @Override
     public void onResume() {
@@ -43,34 +44,11 @@ public class Tab1 extends android.support.v4.app.Fragment {
         System.out.println("In tab1 oncreate view");
         Log.d("Tag","In tab1");
 
-        View v  = inflater.inflate(R.layout.tab1, container, false);
+        v  = inflater.inflate(R.layout.tab1, container, false);
 
         dbHelper = new DBHelper(getContext());
-        Post p = new Post();
-        p.eventTitle = "Free Pizza Party";
-        p.foodType = "Pizza";
-        p.location = "Fountain";
-        p.longitude = 2.0;
-        p.latitude = 2.0;
-        p.time = "20";
-        p.description = "There is lots of good free food time fun here at pizza land";
-
-        Post p2 = new Post();
-        p2.eventTitle = "Free Cookie Party";
-        p2.foodType = "Cookies";
-        p2.location = "Fountain";
-        p2.longitude = 2.0;
-        p2.latitude = 2.0;
-        p2.time = "20";
-        p2.description = "There is lots of good free food time fun here at cookie land";
-
-        ArrayList<Post> posts = new ArrayList<Post>();
-        posts.add(p);
-        posts.add(p2);
-
-        PostAdapter adapter = new PostAdapter(getContext(), posts);
+        listPostDetails();
         ListView listView = (ListView) v.findViewById(R.id.feedList);
-        listView.setAdapter(adapter);
 
         Button addButton = (Button) v.findViewById(R.id.addPost_btn);
 
@@ -87,75 +65,7 @@ public class Tab1 extends android.support.v4.app.Fragment {
                 ad.getDialog().setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialogInterface) {
-                        ArrayList<Post> newPosts = new ArrayList<Post>();
-                        SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-                        // Define a projection that specifies which columns from the database
-                        // you will actually use after this query.
-                        String[] projection = {
-                                "eventTitle",
-                                "foodType",
-                                "location",
-                                "latitude",
-                                "longitude",
-                                "time",
-                                "description"
-                        };
-
-                        // Filter results WHERE "title" = 'My Title'
-                                                //String selection = "*"
-                                                //String[] selectionArgs = { "My Title" };
-
-                        // How you want the results sorted in the resulting Cursor
-                                                // String sortOrder =
-                                                //FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
-
-                        Cursor cursor = db.query(
-                                "FeedMePosts",                     // The table to query
-                                projection,                               // The columns to return
-                                null,                                // The columns for the WHERE clause
-                                null,                            // The values for the WHERE clause
-                                null,                                     // don't group the rows
-                                null,                                     // don't filter by row groups
-                                null                                 // The sort order
-                        );
-
-                        //List itemIds = new ArrayList<>();
-                        while(cursor.moveToNext()) {
-                            String title = cursor.getString(
-                                    cursor.getColumnIndexOrThrow("eventTitle"));
-                            String food = cursor.getString(
-                                    cursor.getColumnIndexOrThrow("foodType"));
-                            String loc = cursor.getString(
-                                    cursor.getColumnIndexOrThrow("location"));
-                            Double lat = cursor.getDouble(
-                                    cursor.getColumnIndexOrThrow("latitude"));
-                            Double lon = cursor.getDouble(
-                                    cursor.getColumnIndexOrThrow("longitude"));
-                            String time = cursor.getString(
-                                    cursor.getColumnIndexOrThrow("time"));
-                            String description = cursor.getString(
-                                    cursor.getColumnIndexOrThrow("description"));
-
-
-                            Post p = new Post();
-                            p.eventTitle = title;
-                            p.foodType = food;
-                            p.location = loc;
-                            p.latitude = lat;
-                            p.longitude = lon;
-                            p.time = time;
-                            p.description = description;
-
-                            newPosts.add(p);
-
-//                            System.out.println(title);
-//                            System.out.println(food);
-                        }
-                        cursor.close();
-                        PostAdapter adapter = new PostAdapter(getContext(), newPosts);
-                        ListView listView = (ListView) getView().findViewById(R.id.feedList);
-                        listView.setAdapter(adapter);
+                        listPostDetails();
                     }
                 });
 
@@ -190,10 +100,7 @@ public class Tab1 extends android.support.v4.app.Fragment {
         String time = post.time;
         String description = post.description;
 
-
         Bundle args = new Bundle();
-
-
         args.putString("eventTitle", eventTitle);
         args.putString("location", location);
         args.putString("foodType",foodType);
@@ -206,7 +113,72 @@ public class Tab1 extends android.support.v4.app.Fragment {
         return f;
     }
 
+    public void listPostDetails() {
+        ArrayList<Post> newPosts = new ArrayList<Post>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                "eventTitle",
+                "foodType",
+                "location",
+                "latitude",
+                "longitude",
+                "time",
+                "description"
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        //String selection = "*"
+        //String[] selectionArgs = { "My Title" };
+
+        // How you want the results sorted in the resulting Cursor
+        // String sortOrder =
+        //FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+
+        Cursor cursor = db.query(
+                "FeedMePosts",                     // The table to query
+                projection,                               // The columns to return
+                null,                                // The columns for the WHERE clause
+                null,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                null                                 // The sort order
+        );
+
+        //List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String title = cursor.getString(
+                    cursor.getColumnIndexOrThrow("eventTitle"));
+            String food = cursor.getString(
+                    cursor.getColumnIndexOrThrow("foodType"));
+            String loc = cursor.getString(
+                    cursor.getColumnIndexOrThrow("location"));
+            Double lat = cursor.getDouble(
+                    cursor.getColumnIndexOrThrow("latitude"));
+            Double lon = cursor.getDouble(
+                    cursor.getColumnIndexOrThrow("longitude"));
+            String time = cursor.getString(
+                    cursor.getColumnIndexOrThrow("time"));
+            String description = cursor.getString(
+                    cursor.getColumnIndexOrThrow("description"));
+
+            Post p = new Post();
+            p.eventTitle = title;
+            p.foodType = food;
+            p.location = loc;
+            p.latitude = lat;
+            p.longitude = lon;
+            p.time = time;
+            p.description = description;
+            newPosts.add(p);
+        }
+        cursor.close();
+        PostAdapter adapter = new PostAdapter(getContext(), newPosts);
+        ListView listView = (ListView) v.findViewById(R.id.feedList);
+        listView.setAdapter(adapter);
+    }
 
 
 }
